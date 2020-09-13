1baab87be80192b1ad117ac8bbf71c123e72fa3a
Harlan Brown <hbrown@nuxeo.com>
2020-09-12 22:02:24 -0500

    listener passes event principal to work class

--- src/main/java/org/nuxeo/sample/SampleListener.java
+++ src/main/java/org/nuxeo/sample/SampleListener.java
@@ -2,7 +2,6 @@ package org.nuxeo.sample;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-//import org.nuxeo.ecm.core.api.NuxeoException;
 import org.nuxeo.ecm.core.event.Event;
 import org.nuxeo.ecm.core.event.EventContext;
 import org.nuxeo.ecm.core.event.EventListener;
@@ -10,24 +9,19 @@ import org.nuxeo.ecm.core.work.api.WorkManager;
 import org.nuxeo.runtime.api.Framework;
 
 public class SampleListener implements EventListener {
-  
+
     private static final Log log = LogFactory.getLog(SampleListener.class);
 
     @Override
     public void handleEvent(Event event) {
-
-        if(event.getName().equalsIgnoreCase("sampleListener")){
-            log.info("Hi from sample listener");
-        } 
-
-        WorkManager workManager = Framework.getService(WorkManager.class);
-
-        if (workManager == null) {
-            throw new RuntimeException("No WorkManager available");
+        EventContext ctx = event.getContext();
+        if(event.getName().equalsIgnoreCase("sampleEvent")){
+            WorkManager workManager = Framework.getService(WorkManager.class);
+            if (workManager == null) {
+                throw new RuntimeException("No WorkManager available");
+            }
+            SampleWork work = new SampleWork(ctx.getPrincipal().getName());
+            workManager.schedule(work);
         }
-
-        SampleWork work = new SampleWork();
-        workManager.schedule(work);
-
     }
 }
--- src/main/java/org/nuxeo/sample/SampleWork.java
+++ src/main/java/org/nuxeo/sample/SampleWork.java
@@ -6,11 +6,17 @@ import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.core.work.AbstractWork;
 import org.nuxeo.runtime.api.Framework;
 
+import org.nuxeo.ecm.core.api.IterableQueryResult;
+
 public class SampleWork extends AbstractWork {
 
     private static final Log log = LogFactory.getLog(SampleWork.class);
 
     public static final String CATEGORY = "sampleWork";
+
+    public SampleWork(String originatingUsername){ 
+        setOriginatingUsername(originatingUsername);
+    }
 
     @Override
     public String getCategory() {
@@ -24,7 +30,15 @@ public class SampleWork extends AbstractWork {
 
     @Override
     public void work() {
-        log.info("HI from sample worker");
+        log.error(originatingUsername);
+
+        openUserSession();
+                
+        IterableQueryResult it = session.queryAndFetch("select * from Document", org.nuxeo.ecm.core.query.sql.NXQL.NXQL); 
+        log.error(it.size());
+        it.close();
+
+
     }
 
 }

967fcdf1d777dccbec0307a95e0c253a2d7ea3ed
Harlan Brown <harlan.brown@hyland.com>
2022-03-14 09:03:03 -0500

    handle-pdf

--- src/main/java/org/nuxeo/sample/SampleListener.java
+++ src/main/java/org/nuxeo/sample/SampleListener.java
@@ -2,9 +2,11 @@ package org.nuxeo.sample;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.event.Event;
 import org.nuxeo.ecm.core.event.EventContext;
 import org.nuxeo.ecm.core.event.EventListener;
+import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 import org.nuxeo.ecm.core.work.api.WorkManager;
 import org.nuxeo.runtime.api.Framework;
 
@@ -15,12 +17,18 @@ public class SampleListener implements EventListener {
     @Override
     public void handleEvent(Event event) {
         EventContext ctx = event.getContext();
-        if(event.getName().equalsIgnoreCase("sampleEvent")){
+        if(event.getName().equalsIgnoreCase("documentCreated")){
+            log.debug("HI FROM SAMPLELISTENER");
+            DocumentEventContext docCtx = (DocumentEventContext) ctx;
+            DocumentModel doc = docCtx.getSourceDocument();
+            log.debug(doc.toString());
+            log.debug(doc.getRepositoryName());
+            log.debug(doc.getId());
             WorkManager workManager = Framework.getService(WorkManager.class);
             if (workManager == null) {
                 throw new RuntimeException("No WorkManager available");
             }
-            SampleWork work = new SampleWork(ctx.getPrincipal().getName());
+            SampleWork work = new SampleWork(doc.getRepositoryName(), doc.getId(), "file:content");
             workManager.schedule(work);
         }
     }
--- src/main/java/org/nuxeo/sample/SampleWork.java
+++ src/main/java/org/nuxeo/sample/SampleWork.java
@@ -5,17 +5,39 @@ import org.apache.commons.logging.LogFactory;
 
 import org.nuxeo.ecm.core.work.AbstractWork;
 import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.core.api.model.Property;
 
-import org.nuxeo.ecm.core.api.IterableQueryResult;
+import org.nuxeo.runtime.transaction.TransactionHelper;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SampleWork extends AbstractWork {
 
     private static final Log log = LogFactory.getLog(SampleWork.class);
 
-    public static final String CATEGORY = "sampleWork";
+    private static final long serialVersionUID = 1L;
 
-    public SampleWork(String originatingUsername){ 
+    public static final String CATEGORY = "sampleWork";
+    private final String FILE_CONTENT = "file:content"; 
+    private final String FILE_FORMAT_TIFF = "image/tiff"; 
+
+    protected final String xpath;
+
+    public SampleWork(String repositoryName, String docId, String xpath){ 
+        super(repositoryName + ':' + docId + ':' + xpath + ":sampleWork");
         setOriginatingUsername(originatingUsername);
+        setDocument(repositoryName, docId);
+        this.xpath = xpath;
     }
 
     @Override
@@ -30,16 +52,60 @@ public class SampleWork extends AbstractWork {
 
     @Override
     public void work() {
-        log.error(originatingUsername);
+        this.setProgress(Progress.PROGRESS_INDETERMINATE);
+        this.setStatus("Converting TIFF to PDF");
 
-        openUserSession();
-                
-        IterableQueryResult it = session.queryAndFetch("select * from Document", org.nuxeo.ecm.core.query.sql.NXQL.NXQL); 
-        log.error(it.size());
-        it.close();
+        List<Blob> tempFiles = new ArrayList<>();
 
+        openSystemSession();
+
+        if (TransactionHelper.isTransactionActiveOrMarkedRollback()) {
+            TransactionHelper.commitOrRollbackTransaction();
+        }
+
+        TransactionHelper.startTransaction();
+
+        try {
+            DocumentModel doc = session.getDocument(new IdRef(id));
+//            Property fileProp = doc.getProperty(FILE_CONTENT);
+//            Blob tifBlob = (Blob) fileProp.getValue();
+//            if (tifBlob != null) {
+//                if (tifBlob.getMimeType().equals(FILE_FORMAT_TIFF)) {
+//                    log.debug("FILE:CONTENT is TIFF, Converting to PDF");
+//                }
+//            }
+        } catch (Exception ex) {
+            log.error("An issue occurred while converting file to PDF", ex);
+//        } finally {
+//            cleanupTempFiles(tempFiles);
+        }
 
     }
 
-}
+    protected void cleanupTempFiles(List<Blob> tempFiles) {
+        for (Blob blob : tempFiles) {
+            if (blob == null) {
+                continue;
+            }
+            File file = blob.getFile();
+            if (file == null || !file.exists()) {
+                continue;
+            }
+            File directory = file.getParentFile();
+            try {
+                Files.delete(file.toPath());
+            } catch (IOException | SecurityException e) {
+                log.error(String.format("Cannot delete temporary file %s", file.getAbsolutePath()), e);
+            }
 
+            if (directory == null || !directory.exists()) {
+                continue;
+            }
+            try {
+                Files.delete(directory.toPath());
+            } catch (IOException | SecurityException e) {
+                log.error(String.format("Cannot delete temporary directory %s", directory.getAbsolutePath()), e);
+            }
+        }
+    }
+}

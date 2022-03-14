be24d1eb4239e99f516237b6d171d789da5cc29f
Harlan Brown <harlan.brown@hyland.com>
2022-03-14 16:48:19 -0500

    ConvertMainBlobToPDF

--- src/main/java/org/nuxeo/sample/SampleWork.java
+++ src/main/java/org/nuxeo/sample/SampleWork.java
@@ -2,8 +2,14 @@ package org.nuxeo.sample;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.automation.AutomationService;
+import org.nuxeo.ecm.automation.OperationContext;
+import org.nuxeo.ecm.automation.OperationException;
+import org.nuxeo.ecm.automation.core.util.Properties;
+import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.core.api.model.Property;
 import org.nuxeo.ecm.core.work.AbstractWork;
 import org.nuxeo.runtime.api.Framework;
 
@@ -15,7 +21,13 @@ public class SampleWork extends AbstractWork {
 
     public static final String CATEGORY = "sampleWork";
 
+    private final String FILE_CONTENT = "file:content"; 
+    private final String FILE_FORMAT_TIFF = "image/tiff";
     private final String xpath;
+
+    protected AutomationService service;
+
+    protected Properties chainParameters = new Properties();
 
     public SampleWork(String repositoryName, String docId, String xpath){ 
         super(repositoryName + ':' + docId + ':' + xpath + ":sampleWork");
@@ -37,13 +49,35 @@ public class SampleWork extends AbstractWork {
     public void work() {
 
         openSystemSession();
+
         if (!session.exists(new IdRef(docId))){
-            log.error("doc ref no good"); 
+
+            return;
+
         } else {
-          log.debug("HI FROM WORKER"); 
-          DocumentModel theDoc = session.getDocument(new IdRef(docId));
+
+            DocumentModel doc = session.getDocument(new IdRef(docId));
+
+            Property fileProp = doc.getProperty(FILE_CONTENT);
+
+            Blob tiffBlob = (Blob) fileProp.getValue();
+
+            if (tiffBlob != null) {
+
+                if (tiffBlob.getMimeType().equals(FILE_FORMAT_TIFF)) {
+
+                    AutomationService service = Framework.getService(AutomationService.class);
+
+                    try { 
+                        OperationContext ctx = new OperationContext(session);
+                        ctx.setInput(doc);
+                        service.run(ctx, "ConvertMainBlobToPDF");
+                    } catch (OperationException oe) {
+                        log.error(oe);
+                    }
+
+                }
+            }
         }
-
     }
-
 }
